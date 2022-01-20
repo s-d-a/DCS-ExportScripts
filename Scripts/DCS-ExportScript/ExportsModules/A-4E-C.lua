53dda63e1cb102823ac89307a6d6aa27678af344
Bailey <dabailey107@gmail.com>
2022-01-20 18:33:21 +0900

    added all a4 args

--- Scripts/DCS-ExportScript/ExportsModules/A-4E-C.lua
+++ Scripts/DCS-ExportScript/ExportsModules/A-4E-C.lua
@@ -14,8 +14,205 @@ ExportScript.ConfigEveryFrameArguments =
 	 [19] = "%0.1f", <- floating-point number with 1 digit after point
 	[129] = "%1d",   <- decimal number
 	  [5] = "%.f",   <- floating point number rounded to a decimal number
-	]]
+]]
+	--there may be precisely 1 duplicate arg
+	[16] = "%0.4f", --mirrors_draw
+	[26] = "%0.4f", --Canopy
+	[129] = "%0.4f", --CanopyLever
 	
+	[2] = "%0.4f", --StickPitch
+	[3] = "%0.4f", --StickBank
+	[4] = "%0.4f", --RudderPedals
+	[5] = "%0.4f", --LeftBrakePedal
+	[6] = "%0.4f", --RightBrakePedal
+	
+	[80] = "%0.4f", --Throttle
+	[870] = "%0.4f", --PitchTrim
+	[871] = "%0.4f", --RollTrim
+	[28] = "%0.4f", --PitchTrimKnob
+	[29] = "%0.4f", --RollTrimKnob
+	
+	[520] = "%0.4f", --Engine_RPM
+	[521] = "%0.4f", --Engine_RPMsub
+	
+	[526] = "%0.4f", --CM_bank1_Xx
+	[527] = "%0.4f", --CM_bank1_xX
+	[528] = "%0.4f", --CM_bank2_Xx
+	[529] = "%0.4f", --CM_bank2_xX
+	
+	[540] = "%0.4f", --EGT
+	[560] = "%0.4f", --Engine_Fuel_Flow
+	[152] = "%0.4f", --OilPressure
+	[151] = "%0.4f", --PressureRatio
+	[105] = "%0.4f", --ManualFuelControl_Warn
+	
+	[23] = "%0.4f", --FlapsIndicator
+	[10] = "%0.4f", --TailhookLever
+	[20] = "%0.4f", --GearNose
+	[21] = "%0.4f", --GearLeft
+	[22] = "%0.4f", --GearRight
+	[27] = "%0.4f", --GearLight
+	
+	[153] = "%0.4f", --HideStick
+	
+	[580] = "%0.4f", --FuelGauge
+	[665] = "%0.4f", --StandbyAttHorizon
+	[880] = "%0.4f", --IASGauge
+	[881] = "%0.4f", --MachDisc
+	[882] = "%0.4f", --IASIndex
+	[883] = "%0.4f", --MachIndex
+	[600] = "%0.4f", --RadarAltimeter
+	[601] = "%0.4f", --LAWS_indexer
+	[604] = "%0.4f", --LAWS_OFF
+	[605] = "%0.4f", --LAWS_light_gauge
+	[150] = "%0.4f", --Oil_light_gauge
+	[856] = "%0.4f", --INDICATOR_BRIGHTNESS
+	[857] = "%0.4f", --AOA_BRIGHTNESS
+	
+	[858] = "%0.4f", --Glareshield_BRIGHTNESS
+	[154] = "%0.4f", --Glareshield_WHEELS
+	[155] = "%0.4f", --Glareshield_LABS
+	[156] = "%0.4f", --Glareshield_LAWS
+	[157] = "%0.4f", --Glareshield_OBST
+	[158] = "%0.4f", --Glareshield_IFF
+	[159] = "%0.4f", --Glareshield_FIRE
+	
+	[780] = "%0.4f", --BDHI_Heading
+	[781] = "%0.4f", --BDHI_Needle1
+	[782] = "%0.4f", --BDHI_Needle2
+	[786] = "%0.4f", --BDHI_DME_Flag
+	[785] = "%0.4f", --BDHI_DME_Xxx
+	[784] = "%0.4f", --BDHI_DME_xXx
+	[783] = "%0.4f", --BDHI_DME_xxX
+	[381] = "%0.4f", --BDHI_ILS_GS
+	[382] = "%0.4f", --BDHI_ILS_LOC
+	
+	[820] = "%0.4f", --Altimeter
+	[821] = "%0.4f", --Altimeter10K
+	[822] = "%0.4f", --Altimeter1K
+	[823] = "%0.4f", --Altimeter100s
+	[824] = "%0.4f", --AltAdjNNxx
+	[825] = "%0.4f", --AltAdjxxNx
+	[826] = "%0.4f", --AltAdjxxxN
+	
+	[710] = "%0.4f", --CabinAlt
+	[760] = "%0.4f", --LiquidOxygen
+	[761] = "%0.4f", --Oxygen_light_gauge
+	[762] = "%0.4f", --Oxygen_flag_gauge
+	
+	[360] = "%0.4f", --Accel_cur
+	[137] = "%0.4f", --Accel_max
+	[138] = "%0.4f", --Accel_min
+	
+	[800] = "%0.4f", --VerticalVelocity
+	
+	[383] = "%0.4f", --ADIPitch
+	[384] = "%0.4f", --ADIRoll
+	[385] = "%0.4f", --ADIHeading
+	[387] = "%0.4f", --ADIOFF
+	[388] = "%0.4f", --ADISlip
+	[389] = "%0.4f", --ADITurn
+	
+	[148] = "%0.4f", --BackupCompass
+	[660] = "%0.4f", --AttGyroStbyPitch
+	[661] = "%0.4f", --AttGyroStbyRoll
+	[664] = "%0.4f", --AttGyroStbyOFF
+	[741] = "%0.4f", --AWRSPower
+	[894] = "%0.4f", --GunsightReflector
+	
+	[859] = "%0.4f", --Ladder_Brightness
+	[860] = "%0.4f", --Ladder_FuelBoostCaution
+	[861] = "%0.4f", --Ladder_ControlHydraulicCaution
+	[862] = "%0.4f", --Ladder_UtilityHydraulicCaution
+	[863] = "%0.4f", --Ladder_FuelTransCaution
+	[864] = "%0.4f", --Ladder_SpdBrkCaution
+	[865] = "%0.4f", --Ladder_SpoilerCaution
+	
+	[866] = "%0.4f", --Advisory_InRange
+	[867] = "%0.4f", --Advisory_SetRange
+	[868] = "%0.4f", --Advisory_Dive
+	[147] = "%0.4f", --APCLight
+	
+	[840] = "%0.4f", --AngleOfAttack
+	[850] = "%0.4f", --AoA_Green
+	[851] = "%0.4f", --AoA_Yellow
+	[852] = "%0.4f", --AoA_Red
+	
+	[406] = "%0.4f", --APG53A_LeftRange
+	[407] = "%0.4f", --APG53A_BottomRange
+	[167] = "%0.4f", --AFCS_HDG_100s
+	[168] = "%0.4f", --AFCS_HDG_10s
+	[169] = "%0.4f", --AFCS_HDG_1s
+	[115] = "%0.4f", --APG53A_Glow
+	[171] = "%0.4f", --Doppler_MemoryLight
+	[172] = "%0.4f", --Doppler_Drift
+	[173] = "%0.4f", --Doppler_Speed_Xnn
+	[174] = "%0.4f", --Doppler_Speed_nXn
+	[175] = "%0.4f", --Doppler_Speed_nnX
+	
+	[178] = "%0.4f", --Nav_CurPos_Lat_Xnnnn
+	[179] = "%0.4f", --Nav_CurPos_Lat_nXnnn
+	[180] = "%0.4f", --Nav_CurPos_Lat_nnXnn
+	[181] = "%0.4f", --Nav_CurPos_Lat_nnnXn
+	[182] = "%0.4f", --Nav_CurPos_Lat_nnnnX
+	[184] = "%0.4f", --Nav_CurPos_Lon_Xnnnnn
+	[185] = "%0.4f", --Nav_CurPos_Lon_nXnnnn
+	[186] = "%0.4f", --Nav_CurPos_Lon_nnXnnn
+	[187] = "%0.4f", --Nav_CurPos_Lon_nnnXnn
+	[188] = "%0.4f", --Nav_CurPos_Lon_nnnnXn
+	[189] = "%0.4f", --Nav_CurPos_Lon_nnnnnX
+	[191] = "%0.4f", --Nav_Dest_Lat_Xnnnn
+	[192] = "%0.4f", --Nav_Dest_Lat_nXnnn
+	[193] = "%0.4f", --Nav_Dest_Lat_nnXnn
+	[194] = "%0.4f", --Nav_Dest_Lat_nnnXn
+	[195] = "%0.4f", --Nav_Dest_Lat_nnnnX
+	[197] = "%0.4f", --Nav_Dest_Lon_Xnnnnn
+	[198] = "%0.4f", --Nav_Dest_Lon_nXnnnn
+	[199] = "%0.4f", --Nav_Dest_Lon_nnXnnn
+	[200] = "%0.4f", --Nav_Dest_Lon_nnnXnn
+	[201] = "%0.4f", --Nav_Dest_Lon_nnnnXn
+	[202] = "%0.4f", --Nav_Dest_Lon_nnnnnX
+	
+	[210] = "%0.4f", --Nav_WindSpeed_Xxx
+	[211] = "%0.4f", --Nav_WindSpeed_xXx
+	[212] = "%0.4f", --Nav_WindSpeed_xxX
+	[214] = "%0.4f", --Nav_WindDir_Xxx
+	[215] = "%0.4f", --Nav_WindDir_xXx
+	[216] = "%0.4f", --Nav_WindDir_xxX
+	[204] = "%0.4f", --Nav_Magvar_Xxxxx
+	[205] = "%0.4f", --Nav_Magvar_xXxxx
+	[206] = "%0.4f", --Nav_Magvar_xxXxx
+	[207] = "%0.4f", --Nav_Magvar_xxxXx
+	[208] = "%0.4f", --Nav_Magvar_xxxxX
+	
+	[362] = "%0.4f", --ARC51_Freq_XXxxx
+	[363] = "%0.4f", --ARC51_Freq_xxXxx
+	[364] = "%0.4f", --ARC51_Freq_xxxXX
+	[371] = "%0.4f", --ARC51_Freq_Preset
+	
+	[111] = "%0.4f", --FloodWhite
+	[114] = "%0.4f", --FloodRed
+	[117] = "%0.4f", --InstLightsPrimary
+	[119] = "%0.4f", --ConsoleLights
+	[440] = "%0.4f", --CurrTime_hours
+	[441] = "%0.4f", --CurrTime_mins
+	[442] = "%0.4f", --CurrTime_secs
+	[144] = "%0.4f", --Stopwatch_mins
+	[145] = "%0.4f", --Stopwatch_secs
+	
+	[514] = "%0.4f", --ECM_TEST
+	[515] = "%0.4f", --ECM_GO
+	[516] = "%0.4f", --ECM_NO_GO
+	[517] = "%0.4f", --ECM_SAM
+	[518] = "%0.4f", --ECM_RPT
+	[519] = "%0.4f", --ECM_STBY
+	[500] = "%0.4f", --ECM_REC
+	[531] = "%0.4f", --ECM_Visibilty
+	
+	[260] = "%0.4f", --AFCS_TEST_ROLL
+	[261] = "%0.4f", --AFCS_TEST_YAW
+	[262] = "%0.4f", --AFCS_TEST_PITCH
+	[113] = "%0.4f", --TEST_PARAM_GAUGE
 }
 
 ExportScript.ConfigArguments = 
@@ -23,7 +220,7 @@ ExportScript.ConfigArguments =
 	--[[
 	arguments for export in low tick interval
 	based on "clickabledata.lua"
-	]]
+]]
 	[8] = "%0.4f",		--Landing Gear Handle	{1,0,1}
 	[10] = "%0.4f",		--Arresting Hook Handle	{1,0,1}
 	[24] = "%0.4f",		--Shoulder Harness Control Handle	{1,0,1}
@@ -195,7 +392,7 @@ function ExportScript.ProcessIkarusDCSConfigHighImportance(mainPanelDevice)
 	ExportScript.Tools.SendData("ExportID", "Format")
 	ExportScript.Tools.SendData(2000, string.format("%7.3f", lUHFRadio:get_frequency()/1000000)) -- <- special function for get frequency data
 	ExportScript.Tools.SendData(2000, ExportScript.Tools.RoundFreqeuncy((UHF_RADIO:get_frequency()/1000000))) -- ExportScript.Tools.RoundFreqeuncy(frequency (MHz|KHz), format ("7.3"), PrefixZeros (false), LeastValue (0.025))
-	]]
+]]
 end
 
 function ExportScript.ProcessDACConfigHighImportance(mainPanelDevice)
@@ -209,7 +406,7 @@ function ExportScript.ProcessDACConfigHighImportance(mainPanelDevice)
 	ExportScript.Tools.SendDataDAC("ExportID", "Format", HardwareConfigID)
 	ExportScript.Tools.SendDataDAC("2000", string.format("%7.3f", UHF_RADIO:get_frequency()/1000000))
 	ExportScript.Tools.SendDataDAC("2000", ExportScript.Tools.RoundFreqeuncy((UHF_RADIO:get_frequency()/1000000))) -- ExportScript.Tools.RoundFreqeuncy(frequency (MHz|KHz), format ("7.3"), PrefixZeros (false), LeastValue (0.025))
-	]]
+]]
 end
 
 -----------------------------------------------------
@@ -228,7 +425,10 @@ function ExportScript.ProcessIkarusDCSConfigLowImportance(mainPanelDevice)
 	ExportScript.Tools.SendData("ExportID", "Format")
 	ExportScript.Tools.SendData(2000, string.format("%7.3f", lUHFRadio:get_frequency()/1000000)) -- <- special function for get frequency data
 	ExportScript.Tools.SendData(2000, ExportScript.Tools.RoundFreqeuncy((UHF_RADIO:get_frequency()/1000000))) -- ExportScript.Tools.RoundFreqeuncy(frequency (MHz|KHz), format ("7.3"), PrefixZeros (false), LeastValue (0.025))
-	]]
+]]
+	
+	ExportScript.CockpitParametersFunction(mainPanelDevice)
+	ExportScript.UHF_radioPresets(mainPanelDevice)
 end
 
 function ExportScript.ProcessDACConfigLowImportance(mainPanelDevice)
@@ -242,14 +442,346 @@ function ExportScript.ProcessDACConfigLowImportance(mainPanelDevice)
 	ExportScript.Tools.SendDataDAC("ExportID", "Format", HardwareConfigID)
 	ExportScript.Tools.SendDataDAC("2000", string.format("%7.3f", UHF_RADIO:get_frequency()/1000000))
 	ExportScript.Tools.SendDataDAC("2000", ExportScript.Tools.RoundFreqeuncy((UHF_RADIO:get_frequency()/1000000))) -- ExportScript.Tools.RoundFreqeuncy(frequency (MHz|KHz), format ("7.3"), PrefixZeros (false), LeastValue (0.025))
-	]]
+]]
 end
 
 ----------------------
 -- Custom Functions --
 ----------------------
 
+function ExportScript.CockpitParametersFunction(mainPanelDevice)
 
+
+--[[ Interesting Parameters
+ADI_HDG:169.449555\
+AFCS_HDG_100s:0.000000\
+AFCS_HDG_10s:0.000000\
+AFCS_HDG_1s:0.000000\
+ALT_ADJ_NNxx:29.000000\
+ALT_ADJ_xxNx:9.000000\
+ALT_ADJ_xxxN:2.000000\
+AOA_GREEN:0.000000\
+AOA_RED:0.000000\
+AOA_YELLOW:0.000000\
+APC_LIGHT:0.000000\
+API_TCN_VALID:0.000000\
+API_TCN_X:0.000000\
+API_TCN_Y:0.000000\
+API_TCN_Z:0.000000\
+APN153-DRIFT-GAUGE:0.000000\
+APN153-DRIFT:0.000000\
+APN153-GS:0.000000\
+APN153-MEMORYLIGHT:1.000000\
+APN153-SPEED-nnX:0.000000\
+APN153-SPEED-nXn:0.000000\
+APN153-SPEED-Xnn:0.000000\
+APN153-WIND-DIR:0.000000\
+APN153-WIND-SPEED:0.000000\
+ARC51-FREQ-PRESET:0.000000\
+ARC51-FREQ-XXxxx:0.150000\
+ARC51-FREQ-xxXxx:0.500000\
+ARC51-FREQ-xxxXX:0.500000\
+ASN41-BEARING:-96.528709\
+ASN41-MAGVAR-Xxxxx:0.000000\
+ASN41-MAGVAR-xXxxx:0.000000\
+ASN41-MAGVAR-xxxXx:0.700000\
+ASN41-MAGVAR-xxXxx:0.700000\
+ASN41-MAGVAR-xxxxX:1.000000\
+ASN41-RANGE:0.611110\
+ASN41-TRACK:263.471291\
+ASN41-VALID:1.000000\
+ASN41-WINDDIR-Xxx:0.000000\
+ASN41-WINDDIR-xXx:0.000000\
+ASN41-WINDDIR-xxX:0.000000\
+ASN41-WINDSPEED-xxX:0.000000\
+ASN41-WINDSPEED-Xxx:0.000000\
+ASN41-WINDSPEED-xXx:0.000000\
+BASE_SENSOR_BAROALT:32.168477\
+BASE_SENSOR_IAS:8.814941\
+BASE_SENSOR_RADALT:2.158444\
+BASE_SENSOR_TAS:8.828558\
+BDHI_DME_FLAG:0.000000\
+BDHI_DME_Xxx:0.000000\
+BDHI_DME_xxX:0.000000\
+BDHI_DME_xXx:0.000000\
+BDHI_HDG:259.449462\
+BDHI_ILS_GS:-1.000000\
+BDHI_ILS_LOC:-1.000000\
+BDHI_NEEDLE1:0.000000\
+BDHI_NEEDLE2:0.000000\
+COMPASS_HDG:190.550445\
+CURRTIME_HOURS:6.755694\
+CURRTIME_MINS:45.341667\
+CURRTIME_SECS:19.921767\
+D_ALT_100S:105.539620\
+D_ALT_10K:105.539620\
+D_ALT_1K:105.539620\
+D_FUEL:2716.651885\
+D_IAS:0.000000\
+FM_AIRSPEED:3.231500\
+FM_GFORCE:0.997350\
+OIL_PRESSURE:40.086509\
+RPM:55.773540\
+STOPWATCH_MINS:0.000000\
+STOPWATCH_SECS:0.000000\
+TACAN_CHANNEL:0.000000\
+UHF_FREQ:255.500000\
+VHF_AM_FREQ:124.000000\
+VVI:0.000013\
+WHEEL_CHOCKS_STATE:0.000000\
+WS_IR_MISSILE_LOCK:0.000000\
+NAV_CURPOS_LAT_nnnnX:0.000000\
+NAV_CURPOS_LAT_nnnXn:0.520457\
+NAV_CURPOS_LAT_nnXnn:0.000000\
+NAV_CURPOS_LAT_nXnnn:0.500000\
+NAV_CURPOS_LAT_Xnnnn:0.400000\
+NAV_CURPOS_LON_nnnnnX:0.500000\
+NAV_CURPOS_LON_nnnnXn:0.726362\
+NAV_CURPOS_LON_nnnXnn:0.500000\
+NAV_CURPOS_LON_nnXnnn:0.800000\
+NAV_CURPOS_LON_nXnnnn:0.300000\
+NAV_CURPOS_LON_Xnnnnn:0.000000\
+NAV_DEST_LAT_nnnnX:0.000000\
+NAV_DEST_LAT_nnnXn:0.521700\
+NAV_DEST_LAT_nnXnn:0.000000\
+NAV_DEST_LAT_nXnnn:0.500000\
+NAV_DEST_LAT_Xnnnn:0.400000\
+NAV_DEST_LON_nnnnnX:0.500000\
+NAV_DEST_LON_nnnnXn:0.639908\
+NAV_DEST_LON_nnnXnn:0.500000\
+NAV_DEST_LON_nnXnnn:0.800000\
+NAV_DEST_LON_nXnnnn:0.300000\
+NAV_DEST_LON_Xnnnnn:0.000000\
+]]
+
+end
+
+
+function ExportScript.UHF_radioPresets(mainPanelDevice)
+
+local a4_list_indication_4 = ExportScript.Tools.split(list_indication(4), "%c")
+
+--[[ Example return
+{
+    [1] = "-----------------------------------------",
+    [2] = "txt_chn[1]",
+    [3] = "255.50",
+    [4] = "-----------------------------------------",
+    [5] = "txt_chn[2]",
+    [6] = "237.30",
+    [7] = "-----------------------------------------",
+    [8] = "txt_chn[3]",
+    [9] = "251.00",
+    [10] = "-----------------------------------------",
+    [11] = "txt_chn[4]",
+    [12] = "254.00",
+    [13] = "-----------------------------------------",
+    [14] = "txt_chn[5]",
+    [15] = "250.00",
+    [16] = "-----------------------------------------",
+    [17] = "txt_chn[6]",
+    [18] = "270.00",
+    [19] = "-----------------------------------------",
+    [20] = "txt_chn[7]",
+    [21] = "257.00",
+    [22] = "-----------------------------------------",
+    [23] = "txt_chn[8]",
+    [24] = "258.00",
+    [25] = "-----------------------------------------",
+    [26] = "txt_chn[9]",
+    [27] = "262.00",
+    [28] = "-----------------------------------------",
+    [29] = "txt_chn[10]",
+    [30] = "259.00",
+    [31] = "-----------------------------------------",
+    [32] = "txt_chn[11]",
+    [33] = "268.00",
+    [34] = "-----------------------------------------",
+    [35] = "txt_chn[12]",
+    [36] = "269.00",
+    [37] = "-----------------------------------------",
+    [38] = "txt_chn[13]",
+    [39] = "260.00",
+    [40] = "-----------------------------------------",
+    [41] = "txt_chn[14]",
+    [42] = "263.00",
+    [43] = "-----------------------------------------",
+    [44] = "txt_chn[15]",
+    [45] = "261.00",
+    [46] = "-----------------------------------------",
+    [47] = "txt_chn[16]",
+    [48] = "267.00",
+    [49] = "-----------------------------------------",
+    [50] = "txt_chn[17]",
+    [51] = "251.00",
+    [52] = "-----------------------------------------",
+    [53] = "txt_chn[18]",
+    [54] = "253.00",
+    [55] = "-----------------------------------------",
+    [56] = "txt_chn[19]",
+    [57] = "266.00",
+    [58] = "-----------------------------------------",
+    [59] = "txt_chn[20]",
+    [60] = "252.00",
+}
+]]
+
+	local UHF_ch1 = a4_list_indication_4[3]
+	local UHF_ch2 = a4_list_indication_4[6]
+	local UHF_ch3 = a4_list_indication_4[9]
+	local UHF_ch4 = a4_list_indication_4[12]
+	local UHF_ch5 = a4_list_indication_4[15]
+	local UHF_ch6 = a4_list_indication_4[18]
+	local UHF_ch7 = a4_list_indication_4[21]
+	local UHF_ch8 = a4_list_indication_4[24]
+	local UHF_ch9 = a4_list_indication_4[27]
+	local UHF_ch10 = a4_list_indication_4[30]
+	local UHF_ch11 = a4_list_indication_4[33]
+	local UHF_ch12 = a4_list_indication_4[36]
+	local UHF_ch13 = a4_list_indication_4[39]
+	local UHF_ch14 = a4_list_indication_4[42]
+	local UHF_ch15 = a4_list_indication_4[45]
+	local UHF_ch16 = a4_list_indication_4[48]
+	local UHF_ch17 = a4_list_indication_4[51]
+	local UHF_ch18 = a4_list_indication_4[54]
+	local UHF_ch19 = a4_list_indication_4[57]
+	local UHF_ch20 = a4_list_indication_4[60]
+												
+	ExportScript.Tools.SendData(2000, "U1 " .. UHF_ch1 
+									.. "\nU2 " .. UHF_ch2 
+									.. "\nU3 " .. UHF_ch3 
+									.. "\nU4 " .. UHF_ch4)
+
+	ExportScript.Tools.SendData(2001, "U5 " .. UHF_ch5 
+									.. "\nU6 " .. UHF_ch6
+									.. "\nU7 " .. UHF_ch7
+									.. "\nU8 " .. UHF_ch8)
+
+	ExportScript.Tools.SendData(2002, "U9 " .. UHF_ch9
+									.. "\nU10 " .. UHF_ch10
+									.. "\nU11 " .. UHF_ch11
+									.. "\nU12 " .. UHF_ch12)
+
+	ExportScript.Tools.SendData(2003, "U13 " .. UHF_ch13
+									.. "\nU14 " .. UHF_ch14
+									.. "\nU15 " .. UHF_ch15
+									.. "\nU16 " .. UHF_ch16)
+
+	ExportScript.Tools.SendData(2004, "U17 " .. UHF_ch17
+									.. "\nU18 " .. UHF_ch18
+									.. "\nU19 " .. UHF_ch19
+									.. "\nU12 " .. UHF_ch20)									
+
+end
+--[[ notes
+n = 1
+return list_indication(n)
+
+
+list_indication(1)
+-----------------------------------------\
+debug_screen\
+\
+children are {\
+-----------------------------------------\
+ias_{D093CE1E-4FB5-4690-B08C-20DD67DEAED3}\
+  0KIAS\
+-----------------------------------------\
+tas_{10829118-99FD-47e4-A037-1D4B1BECA9F5}\
+  0KTAS\
+-----------------------------------------\
+{32CC9A49-7C9D-44c0-9584-C132978873BD}\
+i 0.0\
+-----------------------------------------\
+{1657C7D5-D301-4684-8ABF-2E9E86698559}\
+M 0.00\
+-----------------------------------------\
+{8BAAF7F5-011D-4c7c-89EE-AFFDDCB48965}\
+G 0.0\
+-----------------------------------------\
+{39144282-FEA3-4162-9B00-C5711F7469F0}\
+TH:0.0\
+-----------------------------------------\
+{3DA17F46-CF15-4122-9856-9DC62D282A97}\
+(null)\
+-----------------------------------------\
+{7D0B208A-3549-4bf4-AB7C-55E425D5B18A}\
+(null)\
+-----------------------------------------\
+{E3785616-B6C1-45a2-88E5-ED3A97B33633}\
+(null)\
+-----------------------------------------\
+{3E04EAD0-9E85-4324-9B75-29566B80B5BB}\
+VV:  0\
+-----------------------------------------\
+{78E1D8C3-F18B-4e47-A6B0-072FE74F9CE2}\
+RPM:0.0%\
+}\
+
+
+list_indication(4)
+-----------------------------------------\
+txt_chn[1]\
+255.50\
+-----------------------------------------\
+txt_chn[2]\
+237.30\
+-----------------------------------------\
+txt_chn[3]\
+251.00\
+-----------------------------------------\
+txt_chn[4]\
+254.00\
+-----------------------------------------\
+txt_chn[5]\
+250.00\
+-----------------------------------------\
+txt_chn[6]\
+270.00\
+-----------------------------------------\
+txt_chn[7]\
+257.00\
+-----------------------------------------\
+txt_chn[8]\
+258.00\
+-----------------------------------------\
+txt_chn[9]\
+262.00\
+-----------------------------------------\
+txt_chn[10]\
+259.00\
+-----------------------------------------\
+txt_chn[11]\
+268.00\
+-----------------------------------------\
+txt_chn[12]\
+269.00\
+-----------------------------------------\
+txt_chn[13]\
+260.00\
+-----------------------------------------\
+txt_chn[14]\
+263.00\
+-----------------------------------------\
+txt_chn[15]\
+261.00\
+-----------------------------------------\
+txt_chn[16]\
+267.00\
+-----------------------------------------\
+txt_chn[17]\
+251.00\
+-----------------------------------------\
+txt_chn[18]\
+253.00\
+-----------------------------------------\
+txt_chn[19]\
+266.00\
+-----------------------------------------\
+txt_chn[20]\
+252.00\
+
+]]
 
 ------------------------------
 -- General Helper Functions --
@@ -333,3 +865,11 @@ function getCockpitParam()
 	return TmpReturn
 end
 
+-- split reference
+function ExportScript.Tools.split(stringvalue, delimiter)
+    result = {};
+    for match in (stringvalue..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
