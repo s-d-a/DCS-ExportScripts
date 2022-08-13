12ad161d03b98e3136a35dd2ed41bd794fb27279
Bailey <dabailey107@gmail.com>
2022-08-13 10:42:59 +0900

    Add files via upload

---
+++ Scripts/DCS-ExportScript/ExportsModules/Su-30MKI.lua
@@ -0,0 +1,60 @@
+-- Su-30MKI
+
+ExportScript.FoundDCSModule = true
+ExportScript.Version.Su30MKI = "1.2.1"
+
+ExportScript.ConfigEveryFrameArguments = 
+{
+	--[[
+	every frames arguments
+	based of "mainpanel_init.lua"
+	Example (http://www.lua.org/manual/5.1/manual.html#pdf-string.format)
+	[DeviceID] = "Format"
+	  [4] = "%.4f",  <- floating-point number with 4 digits after point
+	 [19] = "%0.1f", <- floating-point number with 1 digit after point
+	[129] = "%1d",   <- decimal number
+	  [5] = "%.f",   <- floating point number rounded to a decimal number
+	]]
+}
+ExportScript.ConfigArguments = 
+{
+	--[[
+	arguments for export in low tick interval
+	based on "clickabledata.lua"
+	]]
+    [0] = "%.4f",
+    [1] = "%.4f",
+    [2] = "%.4f",
+    [3] = "%.4f",
+    [4] = "%.4f",
+    [5] = "%.4f",
+    [6] = "%.4f",
+}
+
+-----------------------------
+-- HIGH IMPORTANCE EXPORTS --
+-- done every export event --
+-----------------------------
+
+-- Pointed to by ProcessIkarusDCSHighImportance
+function ExportScript.ProcessIkarusDCSConfigHighImportance(mainPanelDevice)
+end
+
+function ExportScript.ProcessDACConfigHighImportance(mainPanelDevice)
+end
+
+-----------------------------------------------------
+-- LOW IMPORTANCE EXPORTS                          --
+-- done every gExportLowTickInterval export events --
+-----------------------------------------------------
+
+-- Pointed to by ExportScript.ProcessIkarusDCSConfigLowImportance
+function ExportScript.ProcessIkarusDCSConfigLowImportance(mainPanelDevice)
+end
+
+function ExportScript.ProcessDACConfigLowImportance(mainPanelDevice)
+end
+
+-----------------------------
+--     Custom functions    --
+-----------------------------
